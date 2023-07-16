fc9ba596201e868255074d742e08341f10764898
chihiros <szrkw2ui2a@gmail.com>
2023-07-17 02:21:03 +0900

    fix: App.tsxの修正

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
 import "./App.css";
+import KeyMonitor from "./Types";
 
 export const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="container">
-      <h1>{count}</h1>
-      <button onClick={() => setCount((count) => count + 1)}>Count</button>
+      <KeyMonitor />
     </div>
   );
 };
